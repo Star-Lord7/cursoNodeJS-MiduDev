ef1487ae60e52a8ec65d40b8e2237dbfc06e8b99
Star-Lord7 <samuelnasa375@gmail.com>
2025-12-28 23:38:25 -0600

    DB remota configurada en API-REST

--- 01-ApiREST/models/mysql/movieModel.js
+++ 01-ApiREST/models/mysql/movieModel.js
@@ -1,11 +1,20 @@
 import mysql from 'mysql2/promise'
+import 'dotenv/config'
+
+// const config = {
+//   host: 'localhost',
+//   user: 'root',
+//   port: 3306,
+//   password: '',
+//   database: 'moviesdb'
+// }
 
 const config = {
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: '',
-  database: 'moviesdb'
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  port: process.env.MYSQL_ADDON_PORT,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB
 }
 
 const connection = await mysql.createConnection(config)
